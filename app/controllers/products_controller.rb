ea5624cc556710255914501463911f2371f85e77
Fabio Freitas <fhcfreitas@gmail.com>
2023-11-29 14:43:07 -0300

    chapter seed, products controller and show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,12 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
   def product_params
     params.require(:article).permit(:photo)
   end
